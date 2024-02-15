a7922ef7ad0d930151ccb57b090ec414e9ccc6ef
Zachary Belles <zacharyb@solution-source.net>
2024-02-15 11:19:54 -0500

    fix broken site

--- src/router.js
+++ src/router.js
@@ -288,7 +288,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	hash: createWebHistory(),
+	history: createWebHistory(),
 	routes,
 });
 router.afterEach((to) => {
--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  devServer: {
-    historyApiFallback: true,
-  },
-  configureWebpack: {
-    resolve: {
-      fallback: {
-        util: require.resolve('util/'),
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-        buffer: require.resolve('buffer/'),
-      },
-    },
-  },
-};
 module.exports = defineConfig({
   devServer: {
     historyApiFallback: true,
