c423d566e3db08b611e712a120ace27f2e70e0d5
Zachary Belles <zacharyb@solution-source.net>
2024-02-15 11:13:56 -0500

    Fix Google SEO Attempt #3

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,4 @@
+const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   devServer: {
     historyApiFallback: true,
@@ -13,3 +14,21 @@ module.exports = {
     },
   },
 };
+module.exports = defineConfig({
+  devServer: {
+    historyApiFallback: true,
+  },
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        util: require.resolve('util/'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/'),
+      },
+    },
+  },
+  publicPath: './',
+  outputDir:'dist',
+  assetsDir:'static'
+})
