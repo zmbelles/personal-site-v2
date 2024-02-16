29b66cf7abce3a08d193e3e36fc49552f5e3de66
Zachary Belles <zacharyb@solution-source.net>
2024-02-16 00:02:28 -0500

    Revert "fix broken site"
    This reverts commit a7922ef7ad0d930151ccb57b090ec414e9ccc6ef.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
+module.exports = {
+  devServer: {
+    historyApiFallback: true,
+  },
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        util: require.resolve('util/'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        buffer: require.resolve('buffer/'),
+      },
+    },
+  },
+};
 module.exports = defineConfig({
   devServer: {
     historyApiFallback: true,
