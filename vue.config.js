be0bb6925075b08bb21963c11dead3839c0d81ec
Zachary Belles <zacharyb@solution-source.net>
2024-02-16 00:02:35 -0500

    Revert "Fix Google SEO Attempt #3"
    This reverts commit c423d566e3db08b611e712a120ace27f2e70e0d5.

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,3 @@
-const { defineConfig } = require('@vue/cli-service')
 module.exports = {
   devServer: {
     historyApiFallback: true,
@@ -14,21 +13,3 @@ module.exports = {
     },
   },
 };
-module.exports = defineConfig({
-  devServer: {
-    historyApiFallback: true,
-  },
-  configureWebpack: {
-    resolve: {
-      fallback: {
-        util: require.resolve('util/'),
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-        buffer: require.resolve('buffer/'),
-      },
-    },
-  },
-  publicPath: './',
-  outputDir:'dist',
-  assetsDir:'static'
-})
